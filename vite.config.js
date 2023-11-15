8d7bedeb2edde80b9901bea2016192b678b02377
amir z <aafz1382@gmail.com>
2023-11-16 00:38:49 +0330

    upd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Pizza-React/",
+  base: "/Pizza-React/",
   plugins: [react(), eslint()],
 })
